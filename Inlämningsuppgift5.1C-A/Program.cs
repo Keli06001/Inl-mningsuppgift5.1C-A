ac2a03e7cf68c055ad3cd98d90f1f84b713143f7
Keli06001 <keli06001@utb.vaxjo.se>
2023-11-29 08:53:34 +0100

    Add project files.

---
+++ Inlämningsuppgift5.1C-A/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Inlämningsuppgift5_1C_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] meningarArray = new string[]
+            {
+            "Detta är den första meningen.",
+            "Här kommer den andra meningen.",
+            "Och här är den tredje meningen."
+            };
+
+            foreach (string mening in meningarArray)
+            {
+                Console.WriteLine(mening + "\n"); 
+            }
+
+        }
+    }
+}
